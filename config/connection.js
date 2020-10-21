ea309fd07c11f6e3bc0f721b552e7841184cbd64
Celine Ross <celineross@gmail.com>
2020-10-21 16:34:10 -0500

    added public folder, updated index handlebars

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: process.env.MYSQLPW,
-  database: "burgers_db"
+  database: "burger_db"
 });
 
 connection.connect(function(err) {
--- models/burger.js
+++ models/burger.js
@@ -2,24 +2,21 @@ var orm = require("../config/orm.js");
 
 var burger = {
     selectAll: function (cb) {
-        orm.selectAll("burgers",
-            function (res) {
-                cb(res);
-            });
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
     },
 
     insertOne: function (val, cb) {
-        orm.insertOne("burgers", "burger_name", val,
-            function (res) {
-                cb(res);
-            });
+        orm.insertOne("burgers", "burger_name", val, function (res) {
+            cb(res);
+        });
     },
 
     updateOne: function (boolean, condition, cb) {
-        orm.updateOne("burgers", "devoured", boolean, condition,
-            function (res) {
-                cb(res);
-            });
+        orm.updateOne("burgers", "devoured", boolean, condition, function (res) {
+            cb(res);
+        });
     }
 }
 
