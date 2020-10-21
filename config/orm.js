d3349ea259d39b9f34aa1ee782d46884c183bb75
Celine Ross <celineross@gmail.com>
2020-10-21 15:15:45 -0500

    boilerplate code added skeleton functions added pasted handlebars files

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function (tableName) {
+
+    },
+
+    insertOne: function (tableName, columnName) {
+
+    },
+
+    updateOne: function (tableName, columnName) {
+
+    }
+}
+
+moduloe.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES
+("Avocado Bacon Cheeseburger", false),
+("Vegan Burger", false),
+("Krabby Patty", false);
