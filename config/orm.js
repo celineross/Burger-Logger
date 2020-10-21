cc5074cdbd692e2476fccac2e0ca233e6eacc7ac
Celine Ross <celineross@gmail.com>
2020-10-21 16:06:54 -0500

    updated controller and configs

--- config/orm.js
+++ config/orm.js
@@ -1,28 +1,84 @@
 var connection = require("./connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
 var orm = {
-    selectAll: function (tableName) {
-        connection.query("SELECT * FROM ??" + tableName + ";",
-            function (err, res) {
-                if (err) throw err;
-                console.log(res);
-            });
+    selectAll: function (tableName, cb) {
+        var queryString = "SELECT * FROM " + tableName + ";";
+        connection.query(queryString, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
     },
 
-    insertOne: function (tableName, columnName) {
-        connection.query("INSERT INTO " + tableName + "VALUES " + columnName + ";",
-            function (err, res) {
-                if (err) throw err;
-                console.log(res);
-            });
+    insertOne: function (tableName, columnName, val, cb) {
+        var queryString = "INSERT INTO" + tableName;
+
+        queryString += " (";
+        queryString += columnName.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(val.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
     },
 
-    updateOne: function (tableName, columnName, boolean, condition) {
-        connection.query("UPDATE" + tableName + "SET" + columnName + " = " + boolean + "WHERE" + condition + ";",
-            function (err, res) {
-                if (err) throw err;
-                console.log(res);
-            });
+    updateOne: function (tableName, columnValues, condition, cb) {
+        var queryString = "UPDATE " + tableName;
+
+        queryString += " SET ";
+        queryString += objToSql(columnValues);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
     }
 }
 
